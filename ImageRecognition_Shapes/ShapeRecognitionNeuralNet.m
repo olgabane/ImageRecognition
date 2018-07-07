e072da7ef152213b7f51018db0f9735d010b3f36
ominkina <ominkina@gmail.com>
2018-07-07 19:19:42 -0400

    Training a neural net to recognize shapes; added code to test predictions on test set

--- ImageRecognition_Shapes/ShapeRecognitionNeuralNet.m
+++ ImageRecognition_Shapes/ShapeRecognitionNeuralNet.m
@@ -1,7 +1,8 @@
-###SUMMARY
+#Goal: Train and test neural network that distinguishes shapes
+#Outcome: 100% accuracy on test set
 
 #load training set
-#Features.m: training set (400 training example, 400 features (pixel intensity values))
+#Features.mat: training set (400 training example, 400 features (pixel intensity values))
 #y: labels
 load Features.mat;
 X = Features;
@@ -85,7 +86,7 @@ end
 
 #TRAIN NEURAL NETWORK
 
-options = optimset('MaxIter', 100); #Try different values
+options = optimset('MaxIter', 20); #Try different values
 lambda = 1;	#Try different values
 
 #Parameters required for fmincg() to train neural network
@@ -117,7 +118,7 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-#TEST THIS NEURAL NETWORK FOR ACCURACY USING COMPUTED WEIGHTS
+#TEST THIS NEURAL NETWORK FOR ACCURACY USING COMPUTED WEIGHTS (On training set)
 
 #sigmoid function
 function g = sigmoid(z)
@@ -143,6 +144,27 @@ y_labs = [repmat(1, 100, 1); repmat(2, 100, 1); repmat(3, 100, 1); repmat(4, 100
 
 accuracy = sum(predictions == y_labs)/size(X,1) * 100
 
-#Notes after implementation
+#Accuracy notes. The real test is the next part - testing on test set rather than training set.
 # 'MaxIter' = 100, lambda = 1 results in accuracy of 100%
 
+#TEST THIS NEURAL NETWORK FOR ACCURACY USING COMPUTED WEIGHTS (On TEST SET); This is the real test of the neural net.
+
+#load test set
+#Features_test.mat: test set (48 training example, 400 features (pixel intensity values))
+#y_test: test set labels
+load Features_test.mat;
+X_test = Features_test;
+load y_test.mat;
+
+predictions_test = predict(Theta1, Theta2, X_test);
+
+#calculate accuracy of prediction (difference between prediction_test matrix and y_test labels matrix)
+#convert labels matrix to vector of 1-4;
+y_test_labs = [repmat(4, 12, 1); repmat(3, 12, 1); repmat(2, 12, 1); repmat(1, 12, 1)];
+
+accuracy = sum(predictions_test == y_test_labs)/size(X_test,1) * 100
+
+#Accuracy notes
+# 'MaxIter' = 100, lambda = 1 results in accuracy of 100% of test set. 
+#How few iterations can training go for to still get high accuracy?
+# 'MaxIter' = 20, lambda =1 results in accuracy of 97.9% (One plus is characterized as triangle)
